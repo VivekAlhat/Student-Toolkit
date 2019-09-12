e0759dc596ac74c8619ede0498def756b53cbf9d
VivekAlhat <vivekalhat14@outlook.com>
2019-09-12 15:40:09 +0530

    #1 Commit

---
+++ app/src/main/java/com/example/studenttoolkit/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.studenttoolkit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import static com.example.studenttoolkit.R.layout.activity_main;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
