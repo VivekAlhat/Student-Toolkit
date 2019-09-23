612c5ba699b387bf63ef4a4cfd795aa157897f45
VivekAlhat <vivekalhat14@outlook.com>
2019-09-23 23:23:30 +0530

    Icon Update

--- app/src/main/java/com/example/studenttoolkit/MainActivity.java
+++ app/src/main/java/com/example/studenttoolkit/MainActivity.java
@@ -12,20 +12,20 @@ import static com.example.studenttoolkit.R.layout.activity_main;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     // Cardview objects creation
-    private CardView notesCard,calcCard,errorCard,helpCard;
+    private CardView convCard,calcCard,errorCard,helpCard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Defining individual cards
-        notesCard = (CardView) findViewById(R.id.notes);
+        convCard = (CardView) findViewById(R.id.converter);
         calcCard = (CardView) findViewById(R.id.calculator);
         errorCard = (CardView) findViewById(R.id.err404);
         helpCard = (CardView) findViewById(R.id.help);
 
         // On click listener
-        notesCard.setOnClickListener(this);
+        convCard.setOnClickListener(this);
         calcCard.setOnClickListener(this);
         errorCard.setOnClickListener(this);
         helpCard.setOnClickListener(this);
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         Intent i;
         switch (view.getId()){
-            case R.id.notes : i = new Intent(this,notes.class); startActivity(i); break;
+            case R.id.converter : i = new Intent(this,converter.class); startActivity(i); break;
             case R.id.calculator : i = new Intent(this,calculator.class); startActivity(i); break;
             case R.id.err404 : i = new Intent(this,error404.class); startActivity(i); break;
             case R.id.help : i = new Intent(this,help.class); startActivity(i); break;
--- app/src/main/java/com/example/studenttoolkit/converter.java
+++ app/src/main/java/com/example/studenttoolkit/converter.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class notes extends AppCompatActivity {
+public class converter extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notes);
+        setContentView(R.layout.activity_converter);
     }
 }
